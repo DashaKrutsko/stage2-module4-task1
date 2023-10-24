782e55d0c7c44dadf31bea67f5207c47dffce304
DK <dasha.krutsko@gmail.com>
2023-10-24 12:17:13 +0300

    version 1

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,14 +1,14 @@
 package com.mjc.stage2.impl;
 
+
 import com.mjc.stage2.ConnectionFactory;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
-
-import static jdk.xml.internal.SecuritySupport.getClassLoader;
 
 public class H2ConnectionFactory implements ConnectionFactory {
     private String driver;
@@ -21,11 +21,11 @@ public class H2ConnectionFactory implements ConnectionFactory {
         Connection connection = null;
         Properties properties = new Properties();
         try {
-            properties.load(getClassLoader().getResourceAsStream("app.properties"));
-            driver = properties.getProperty("postgres.driver");
-            url = properties.getProperty("postgres.url");
-            password = properties.getProperty("postgres.password");
-            name = properties.getProperty("postgres.name");
+            properties.load(DataSource.class.getClassLoader().getResourceAsStream("app.properties"));
+            driver = properties.getProperty("jdbc_driver");
+            url = properties.getProperty("db_url");
+            password = properties.getProperty("password");
+            name = properties.getProperty("user");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,7 +35,6 @@ public class H2ConnectionFactory implements ConnectionFactory {
             throwables.printStackTrace();
         }
         return connection;
-
     }
 }
 
